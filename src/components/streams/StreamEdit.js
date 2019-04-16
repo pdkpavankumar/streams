32b99dc96b7ad05c89cc7045be5c54db64f406d9
Perumalla Pavan Kumar <ppavankumar@deloitte.com>
2019-04-16 15:22:34 +0530

    added update stream logic

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,38 @@
+import _ from 'lodash';
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  );
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-export default StreamEdit;
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.stream.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return (
+        <div> Loading ...</div>
+      );
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default (connect(mapStateToProps, { fetchStream, editStream }))(StreamEdit);
