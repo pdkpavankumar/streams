5e9df23a8614e3b0aa0d2d7738bf38bfe20b1c82
Perumalla Pavan Kumar <ppavankumar@deloitte.com>
2019-04-17 15:10:39 +0530

    added logic for streaming

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,19 +1,46 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
   }
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
   render() {
-    if(!this.props.stream) {
+    if (!this.props.stream) {
       return (
         <div>Loading...</div>
       );
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '70%' }} controls />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
